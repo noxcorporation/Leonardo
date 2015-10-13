467cd874183761b07201c8c282d4c499dfcda9e4
nox282 <nox.tux@gmail.com>
2015-10-13 19:27:29 -0400

    update how to

--- learning/windowExample.cpp
+++ learning/windowExample.cpp
@@ -3,25 +3,27 @@
 
 using namespace std;
 
+//How to: Creer une fenetre
+
 int main (int argc, char **argv) {
-	SDL_Window *window;
+	SDL_Window *window;   		//pointeur window	
 
-        SDL_Init (SDL_INIT_VIDEO);
+        SDL_Init (SDL_INIT_VIDEO);  	//Initialisation de la SDL
 
-        window = SDL_CreateWindow(
-		"Yo",
-		500,
-		500,
-		640,
-		480,
-		SDL_WINDOW_OPENGL
+        window = SDL_CreateWindow(	//Methode creer fenetre
+		"Yo",			//titre
+		500,			//position x initiale
+		500,			//position y initiale
+		640,			//taille x
+		480,			//taille y
+		SDL_WINDOW_OPENGL	//flags
 	);
 	
-	if (window == NULL) {
+	if (window == NULL) {		//fool proof
 		cout << "Could not resolve" << endl;
 	} else {
-		SDL_Delay(3000);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
+		SDL_Delay(3000);	//pause de 3 secondes
+		SDL_DestroyWindow(window);//fermer la fenetre
+		SDL_Quit();		//quitter SDL /!\
 	}
 }
