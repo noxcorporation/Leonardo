a63406201ab9a2d7c412c2c4c3161f9f5b9f91b9
Tahj42 <lordtahj@gmail.com>
2015-10-13 15:40:21 -0400

    Setting up
    Basic project files added.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Tahj
+ *
+ * Created on 13 octobre 2015, 11:32
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
