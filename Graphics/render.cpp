c210dfd2ae8ecf16d8fa855a19a99106de3a8631
Tahj42 <lordtahj@gmail.com>
2015-10-15 20:17:14 -0400

    Expanded class Window

--- Graphics/render.cpp
+++ Graphics/render.cpp
@@ -1,23 +1,26 @@
 #include <SDL2/SDL.h>
-#include <iostream>
-
-using namespace std;
 
 class Window {
 	private:
-		SDL_Window *window;
+		SDL_Window* window;
 		SDL_Event event;
 	public:
 		Window();
+		~Window();
 };
 
-Window::Window () {
-    window = SDL_CreateWindow(
-        "Leonardo",
-        500,
-        500,
-        640,
-        480,
-	SDL_WINDOW_OPENGL
-    );
+Window::Window() {
+	window = SDL_CreateWindow(
+		"Leonardo",
+		500,
+		500,
+		640,
+		480,
+		SDL_WINDOW_OPENGL
+	);
+}
+
+Window::~Window() {
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
