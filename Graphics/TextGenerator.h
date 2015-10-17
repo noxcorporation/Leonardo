4faa4be9ef0841752f5885a16a78a85768ff95bd
Tahj42 <lordtahj@gmail.com>
2015-10-16 20:50:29 -0400

    Formatting

--- Graphics/TextGenerator.h
+++ Graphics/TextGenerator.h
@@ -1,19 +1,20 @@
 class TextGenerator {
 	private:
+		int posX;
+		int posY;
 		String message;
 		TTF_Font* font;
 		SDL_Color color;
+		
 		SDL_Surface* surfaceMessage;
 		SDL_Texture* message;
-		int posX;
-		int posY;
 	protected:
 		/*
-                 * Pushes The text into the renderer buffer
-                 **/
-                void renderText(SDL_Renderer renderer);
+		 * Pushes The text into the renderer buffer
+		 **/
+		void renderText(SDL_Renderer renderer);
 	public:
-		TextGenerator(String font, int size, SDL_Color color, int x, int y);
+		TextGenerator(String font, int fontSize, SDL_Color color, int x, int y);
 		~TextGenerator();
 		String getMessage();
 		int getPosX();
