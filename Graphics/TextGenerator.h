07a2e96940c540e5f1b7266a0f83d04def0df2c7
Nicolas HAMARD <nox.tux@gmail.com>
2015-10-16 22:47:00 -0400

    Fixing Issues

--- Graphics/TextGenerator.h
+++ Graphics/TextGenerator.h
@@ -2,20 +2,21 @@ class TextGenerator {
 	private:
 		int posX;
 		int posY;
-		String message;
+		String textMessage;
 		TTF_Font* font;
 		SDL_Color color;
 		
-		SDL_Surface* surfaceMessage;
+		SDL_Rect messageRekt;
 		SDL_Texture* message;
+		SDL_Renderer* renderer;
 	protected:
 		/*
 		 * Pushes The text into the renderer buffer
 		 **/
-		void renderText(SDL_Renderer renderer);
-		SDL_Rect createRectTexture();
+		void renderText();
+		SDL_Rect createRektTexture();
 	public:
-		TextGenerator(String message, String font, int fontSize, SDL_Color color, int x, int y);
+		TextGenerator(String message, String font, int fontSize, SDL_Color color, int x, int y, SDL_Renderer* renderer);
 		~TextGenerator();
 		String getMessage();
 		int getPosX();
