24f428dbdff067e03532b31c538fbc9b8c984a86
Nicolas HAMARD <nox.tux@gmail.com>
2015-10-18 23:51:44 -0400

    Updating Text.h regarding the abstract class between Image and Text

--- Graphics/Text.h
+++ Graphics/Text.h
@@ -1,28 +1,22 @@
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
-#include <iostream>
 
 class Text {
 	private:
-		int posX;
-		int posY;
-		std::string textMessage;
+		int width;
+                int height;
+            
+                char* textMessage;
 		TTF_Font* font;
 		SDL_Color color;
 		
-		SDL_Rect messageRekt;
 		SDL_Texture* message;
-		SDL_Renderer* renderer;
 	protected:
-		/*
-		 * Pushes The text into the renderer buffer
-		 **/
-		void renderText();
-		SDL_Rect createRektTexture(SDL_Renderer* renderer);
 	public:
-		Text(std::string messageIn, char* fontIn, int fontSize, SDL_Color colorIn, int x, int y, SDL_Renderer* renderer);
+		Text(char* messageIn, char* fontIn, int fontSize, SDL_Color colorIn, int x, int y, SDL_Renderer* renderer);
                 ~Text();
-		std::string getMessage();
-		int getPosX();
-		int getPoxY();
-		void setPosXY(int x, int y);
+		char* getMessage();
+                
+                int getW();
+                int getH();
 };
