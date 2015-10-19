d48d7c46297875bafb65c29d28ce75caa1c8722d
Tahj42 <lordtahj@gmail.com>
2015-10-19 12:59:29 -0400

    Moved lib inclusions and program constants to Render.h

--- Graphics/Render.h
+++ Graphics/Render.h
@@ -1,7 +1,18 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include <string>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 
 #define LEONARDO_WINDOW_WIDTH 640
 #define LEONARDO_WINDOW_HEIGHT 480
+
+#define LEONARDO_COLOR_WHITE {255, 255, 255, 255}
+#define LEONARDO_COLOR_BLACK {0, 0, 0, 255}
+
+#define LEONARDO_FONT_QUARMIC "../Assets/Qarmic_sans_Abridged.ttf"
 
 
 class Window {
@@ -29,3 +40,5 @@ class Window {
 		~Window();
 		int cycle();
 };
+
+#endif
