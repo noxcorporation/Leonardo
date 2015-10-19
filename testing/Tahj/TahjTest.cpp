3d9465577d8036831c9a6b10ef808f274c097da8
Tahj42 <lordtahj@gmail.com>
2015-10-19 13:00:41 -0400

    Test code for text display.

--- testing/Tahj/TahjTest.cpp
+++ testing/Tahj/TahjTest.cpp
@@ -6,8 +6,21 @@
  * Testing code to display basic things on screen.
  **/
 void TahjTest::drawScreen(SDL_Renderer* renderer) {
+	ruby(renderer);
+}
+
+void TahjTest::orange(SDL_Renderer* renderer) {
 	//Loading test image.
 	Sprite sprite(renderer, std::string("../Assets/Text.png"));
+	sprite.center();
+	sprite.render(renderer);
+}
+
+void TahjTest::ruby(SDL_Renderer* renderer) {
+	std::string text = "Press DOWN ARROW to close.";
+	SDL_Color color = LEONARDO_COLOR_WHITE;
+	
+	Sprite sprite(renderer, text, color);
 	sprite.center();
 	sprite.render(renderer);
 }
--- testing/Tahj/TahjTest.h
+++ testing/Tahj/TahjTest.h
@@ -1,9 +1,9 @@
-#include <SDL2/SDL.h>
-
-
 class TahjTest {
 	private:
 		Sprite sprite;
+	protected:
+		static void orange(SDL_Renderer*);
+		static void ruby(SDL_Renderer*);
 	public:
 		static void drawScreen(SDL_Renderer*);
 };
