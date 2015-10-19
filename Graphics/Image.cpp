9e25b2badb0d92997339cf583eb6891a5e279eb6
Nicolas HAMARD <nox.tux@gmail.com>
2015-10-19 02:19:26 -0400

    Cleaning Text.h, added New classes in Image. New code design :    * FromFiles : public Image --> Will create SDL_Texture object from     image files.    * FromText : public Image --> Will Create SDL_Texture object from     text string.

--- Graphics/Image.cpp
+++ Graphics/Image.cpp
@@ -1,31 +1,51 @@
-#include <SDL2/SDL_image.h>
 #include "Image.h"
 
-
-/*
- * This constructor loads an image from a file to a texture.
- **/
-Image::Image(SDL_Renderer* renderer, std::string imageFile) {
-	SDL_Surface* surface;
-	surface = IMG_Load(imageFile.c_str());
-	sizeW = surface->w;
-	sizeH = surface->h;
-	texture = SDL_CreateTextureFromSurface(renderer, surface);
-	SDL_FreeSurface(surface);
-}
-
-Image::~Image() {
-	SDL_DestroyTexture(texture);
-}
-
+//class Image
 SDL_Texture* Image::getTexture() {
-	return texture;
+    return texture;
 }
 
 int Image::getW() {
-	return sizeW;
+    return sizeW;
 }
 
 int Image::getH() {
-	return sizeH;
+    return sizeH;
+}
+
+void Image::createTexture(SDL_Renderer* renderer, SDL_Surface* surface) {
+    sizeW = surface->w;
+    sizeH = surface->h;
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+}
+
+//class FromFiles
+FromFiles::FromFiles(SDL_Renderer* renderer, std::string imageFile) {
+    SDL_Surface* surface;
+    surface = IMG_Load(imageFile.c_str());
+    createTexture(renderer, surface);    
+}
+
+FromFiles::~FromFiles() {
+    SDL_DestroyTexture(texture);
+}
+
+//Class FromText
+FromText::FromText(SDL_Renderer* renderer, std::string textIn, std::string fontIn, int fontSize, SDL_Color colorIn) {
+    font = TTF_OpenFont(fontIn.c_str(), fontSize);
+    color = colorIn;
+    text = textIn;
+    SDL_Surface* surface;
+    surface = TTF_RenderText_Blended(font, text.c_str(), color);
+    createTexture(renderer, surface);
+}
+
+FromText::~FromText() {
+    TTF_CloseFont (font);
+    SDL_DestroyTexture(texture);
+}
+
+std::string FromText::getText() {
+    return text;
 }
--- Graphics/Image.h
+++ Graphics/Image.h
@@ -1,17 +1,56 @@
-#include <string>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <string>
 
-
+/*
+ * Abstract class, Text and ImageFiles will extends from it. 
+ **/
 class Image {
-	private:
-		SDL_Texture* texture;
-		int sizeW;
-		int sizeH;
-	protected:
-	public:
-		Image(SDL_Renderer*, std::string);
-		~Image();
-		SDL_Texture* getTexture();
-		int getW();
-		int getH();
+    protected:
+        /*
+         * End product is the SDL_Texture object.
+         **/
+        SDL_Texture* texture;
+
+        int sizeW;
+        int sizeH;
+        
+        void createTexture(SDL_Renderer* renderer, SDL_Surface* surface);
+    public:
+        SDL_Texture* getTexture();
+        
+        int getW();
+        int getH();
+};
+
+/*
+ * SDL_Texture object from a file
+ **/
+class FromFiles : public Image {
+    public:
+        /*
+         * renderer is a pointer to the current renderer object,
+         * imageFile is the path to the source File.
+         **/
+        FromFiles(SDL_Renderer* renderer, std::string imageFile);
+        ~FromFiles();
+};
+
+/*
+ * SDL_Texture object from a string.
+ **/
+class FromText : public Image {
+    private:
+        std::string text;
+        TTF_Font* font;
+        SDL_Color color;
+    public:
+        /*
+         * renderer must the pointer to the current renderer object
+         * textIn is the string to display, fontIn must be the path to the font.ttf file, colorIn is an SDL_Color struct
+         **/
+        FromText(SDL_Renderer* renderer, std::string textIn, std::string fontIn, int fontSize, SDL_Color colorIn);
+        ~FromText();
+        std::string getText();
 };
