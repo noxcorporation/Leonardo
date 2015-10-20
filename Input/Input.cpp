798b42341d010deed8687ddeba41a01619a30d62
Tahj42 <lordtahj@gmail.com>
2015-10-20 15:51:24 -0400

    Updated input map.

--- Input/Input.cpp
+++ Input/Input.cpp
@@ -10,24 +10,32 @@ void Input::processQueue(int* gameScreen) {
 	 **/
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
-		switch (*gameScreen) {
-			case LEONARDO_SCREEN_1:
-				if (event.key.keysym.sym == SDLK_DOWN)		// Down arrow to progress.
-					*gameScreen = LEONARDO_SCREEN_2;
-				break;
-				
-			case LEONARDO_SCREEN_2:
-				if (event.key.keysym.sym == SDLK_RETURN)	// Enter to quit.
-					*gameScreen = LEONARDO_SCREEN_EXIT;
-				if (event.key.keysym.sym == SDLK_a)			// A to progress.
-					*gameScreen = LEONARDO_SCREEN_LEVEL;
-				break;
-				
-			case LEONARDO_SCREEN_LEVEL:
-				if (event.key.keysym.sym == SDLK_ESCAPE)	// Escape to quit.
-					*gameScreen = LEONARDO_SCREEN_EXIT;
-				break;
-		}
+		
+		if (event.key.state == SDL_PRESSED)
+			switch (*gameScreen) {
+				case LEONARDO_SCREEN_1:
+					if (event.key.keysym.sym == SDLK_BACKSPACE)	// Backspace to go back.
+						*gameScreen = LEONARDO_SCREEN_EXIT;
+					if (event.key.keysym.sym == SDLK_DOWN)		// Down arrow to progress.
+						*gameScreen = LEONARDO_SCREEN_2;
+					break;
+					
+				case LEONARDO_SCREEN_2:
+					if (event.key.keysym.sym == SDLK_BACKSPACE)	// Backspace to go back.
+						*gameScreen = LEONARDO_SCREEN_1;
+					if (event.key.keysym.sym == SDLK_RETURN)	// Enter to quit.
+						*gameScreen = LEONARDO_SCREEN_EXIT;
+					if (event.key.keysym.sym == SDLK_a)			// A to progress.
+						*gameScreen = LEONARDO_SCREEN_LEVEL;
+					break;
+					
+				case LEONARDO_SCREEN_LEVEL:
+					if (event.key.keysym.sym == SDLK_BACKSPACE)	// Backspace to go back.
+						*gameScreen = LEONARDO_SCREEN_2;
+					if (event.key.keysym.sym == SDLK_ESCAPE)	// Escape to quit.
+						*gameScreen = LEONARDO_SCREEN_EXIT;
+					break;
+			}
 		
 		if (event.type == SDL_QUIT)							// Close to quit.
 			*gameScreen = LEONARDO_SCREEN_EXIT;
