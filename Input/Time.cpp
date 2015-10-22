5b504dc30281ba13df600ec87cda28dc613d61bd
Tahj42 <lordtahj@gmail.com>
2015-10-21 23:25:36 -0400

    Added Time.h and .cpp for time management.

---
+++ Input/Time.cpp
@@ -0,0 +1,74 @@
+#include "Time.h"
+
+
+Duration::Duration(auto in) {
+	value = in;
+}
+
+void Duration::operator =(auto in) {
+	value = in;
+}
+
+bool Duration::operator ==(auto in) {
+	return value.count() == in;
+}
+
+bool Duration::operator !=(auto in) {
+	return value.count() != in;
+}
+
+bool Duration::operator >(auto in) {
+	return value.count() > in;
+}
+
+bool Duration::operator <(auto in) {
+	return value.count() < in;
+}
+
+bool Duration::operator >=(auto in) {
+	return value.count() >= in;
+}
+
+bool Duration::operator <=(auto in) {
+	return value.count() <= in;
+}
+
+float operator /(int A, Duration B) {
+	return A / B.value.count();
+}
+
+
+
+
+
+TimePoint::TimePoint() {
+	value = steady_clock::now();
+}
+
+Duration operator -(TimePoint A, TimePoint B) {
+	return duration_cast<milliseconds>(A.value - B.value);
+}
+
+
+
+
+
+Clock::Clock(int actionsPerSecond) {
+	updatesPerSecond = actionsPerSecond;
+}
+
+bool Clock::update() {
+	TimePoint now;
+	Duration currentDelay = now - lastUpdate;
+	
+	if (currentDelay >= (1000 / updatesPerSecond)) {
+		lastUpdate = now;
+		updateDelay = currentDelay;
+		return true;
+	} else
+		return false;
+}
+
+int Clock::getActionsPerSecond() {
+	return 1000 / updateDelay;
+}
---
+++ Input/Time.h
@@ -0,0 +1,51 @@
+#include "Leonardo.h"
+
+
+// This class is a custom type.
+// Defines a custom duration type similar to std::chrono::milliseconds.
+class Duration {
+	public:
+		milliseconds value;
+		
+		Duration() {};
+		
+		// Assignment operator overload.
+		Duration(auto);
+		void operator=(auto);
+		
+		// Comparison operator overload.
+		bool operator==(auto);
+		bool operator!=(auto);
+		bool operator>(auto);
+		bool operator<(auto);
+		bool operator>=(auto);
+		bool operator<=(auto);
+};
+
+float operator /(int, Duration);
+
+
+
+// This class is a custom type.
+// Defines a custom time point similar to std::chrono::time_point.
+class TimePoint {
+	public:
+		time_point<steady_clock> value;
+		
+		TimePoint();
+};
+
+Duration operator-(TimePoint, TimePoint);
+
+
+
+class Clock {
+	private:
+		int updatesPerSecond;
+		TimePoint lastUpdate;
+		Duration updateDelay;
+	public:
+		Clock(int);
+		bool update();
+		int getActionsPerSecond();
+};
